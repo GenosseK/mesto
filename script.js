e14358f2cc8827881f13a66dcff5392882b106c7
Alexey Kazhanov <akazhanov72@yandex.ru>
2023-02-02 17:48:42 +0500

    Popup JavaScript

---
+++ script.js
@@ -0,0 +1,31 @@
+const popup = document.querySelector(".popup");
+const closeButton = document.querySelector(".popup__btn-close");
+
+closeButton.addEventListener("click", function() {
+  popup.classList.remove("popup__opened");
+  popup.classList.add("popup");
+});
+
+const editButton = document.querySelector('.profile__edit-button');
+
+editButton.addEventListener('click', function() {
+  popup.classList.remove('popup');
+  popup.classList.add('popup__opened');
+});
+
+const saveButton = document.querySelector('.popup__btn-save');
+const nameInput = document.querySelector('#name');
+const descriptionInput = document.querySelector('#description');
+const profileName = document.querySelector('.profile__name');
+const profileDescription = document.querySelector('.profile__job');
+
+saveButton.addEventListener('click', function (event) {
+  event.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileDescription.textContent = descriptionInput.value;
+  popup.classList.remove('popup__opened');
+});
+
+closeButton.addEventListener('click', function () {
+  popup.classList.remove('popup__opened');
+});
