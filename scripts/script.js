61e36816965e2215e8e71285e9f764d8207976c2
Alexey Kazhanov <akazhanov72@yandex.ru>
2023-02-07 22:34:29 +0500

    Mistakes fixed

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,7 @@ const popup = document.querySelector(".popup");
 const closeButton = document.querySelector(".popup__btn-close");
 
 closeButton.addEventListener("click", function () {
-  popup.classList.remove("popup__opened");
-  popup.classList.add("popup");
+  popup.classList.remove("popup_opened");
 });
 
 const editButton = document.querySelector('.profile__edit-button');
@@ -11,8 +10,7 @@ const editButton = document.querySelector('.profile__edit-button');
 editButton.addEventListener('click', function () {
   nameInput.value = profileName.textContent;
   descriptionInput.value = profileDescription.textContent;
-  popup.classList.remove('popup');
-  popup.classList.add('popup__opened');
+  popup.classList.add('popup_opened');
 });
 
 const saveButton = document.querySelector('.popup__btn-save');
@@ -21,20 +19,38 @@ const descriptionInput = document.querySelector('#description');
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__job');
 
+nameInput.addEventListener('input', function () {
+  if (nameInput.value.length >= 2 && nameInput.value.length < 40 && descriptionInput.value.length >= 2 && descriptionInput.value.length < 40) {
+    saveButton.removeAttribute('disabled');
+    saveButton.classList.remove('popup__btn-save_disabled');
+  } else {
+    saveButton.setAttribute('disabled', true);
+    saveButton.classList.add('popup__btn-save_disabled');
+  }
+});
+
+descriptionInput.addEventListener('input', function () {
+  if (nameInput.value.length >= 2 && nameInput.value.length < 40 && descriptionInput.value.length >= 2 && descriptionInput.value.length < 40) {
+    saveButton.removeAttribute('disabled');
+    saveButton.classList.remove('popup__btn-save_disabled');
+  } else {
+    saveButton.setAttribute('disabled', true);
+    saveButton.classList.add('popup__btn-save_disabled');
+  }
+});
 
 saveButton.addEventListener('click', function (event) {
   event.preventDefault();
   if (!saveButton.classList.contains('popup__btn-save_disabled')) {
     profileName.textContent = nameInput.value;
     profileDescription.textContent = descriptionInput.value;
-    popup.classList.remove('popup__opened');
-    popup.classList.add("popup");
+    popup.classList.remove('popup_opened');
   }
 });
 
 
 closeButton.addEventListener('click', function () {
-  popup.classList.remove('popup__opened');
+  popup.classList.remove('popup_opened');
 });
 
 const likeButton = document.querySelectorAll('.element__button-like');
