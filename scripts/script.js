a253afc3bb59cfcc810b5daba58c91818ea9d583
Alexey Kazhanov <akazhanov72@yandex.ru>
2023-02-08 18:53:00 +0500

    Fixes

--- scripts/script.js
+++ scripts/script.js
@@ -1,63 +1,38 @@
 const popup = document.querySelector(".popup");
-const closeButton = document.querySelector(".popup__btn-close");
-
-closeButton.addEventListener("click", function () {
-  popup.classList.remove("popup_opened");
-});
-
-const editButton = document.querySelector('.profile__edit-button');
-
-editButton.addEventListener('click', function () {
-  nameInput.value = profileName.textContent;
-  descriptionInput.value = profileDescription.textContent;
-  popup.classList.add('popup_opened');
-});
-
-const saveButton = document.querySelector('.popup__btn-save');
+const buttonClose = document.querySelector(".popup__btn-close");
+const buttonEdit = document.querySelector('.profile__edit-button');
 const nameInput = document.querySelector('#name');
 const descriptionInput = document.querySelector('#description');
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__job');
+const buttonLike = document.querySelectorAll('.element__button-like');
+const popupForm = document.querySelector('.popup__form');
 
-nameInput.addEventListener('input', function () {
-  if (nameInput.value.length >= 2 && nameInput.value.length < 40 && descriptionInput.value.length >= 2 && descriptionInput.value.length < 40) {
-    saveButton.removeAttribute('disabled');
-    saveButton.classList.remove('popup__btn-save_disabled');
-  } else {
-    saveButton.setAttribute('disabled', true);
-    saveButton.classList.add('popup__btn-save_disabled');
-  }
-});
+function closePopup() {
+  popup.classList.remove("popup_opened");
+}
 
-descriptionInput.addEventListener('input', function () {
-  if (nameInput.value.length >= 2 && nameInput.value.length < 40 && descriptionInput.value.length >= 2 && descriptionInput.value.length < 40) {
-    saveButton.removeAttribute('disabled');
-    saveButton.classList.remove('popup__btn-save_disabled');
-  } else {
-    saveButton.setAttribute('disabled', true);
-    saveButton.classList.add('popup__btn-save_disabled');
-  }
-});
+buttonClose.addEventListener("click", closePopup);
 
-saveButton.addEventListener('click', function (event) {
+function editPopup() {
+  nameInput.value = profileName.textContent;
+  descriptionInput.value = profileDescription.textContent;
+  popup.classList.add('popup_opened');
+}
+
+buttonEdit.addEventListener('click', editPopup);
+
+function formSubmitHandler(event) {
   event.preventDefault();
-  if (!saveButton.classList.contains('popup__btn-save_disabled')) {
-    profileName.textContent = nameInput.value;
-    profileDescription.textContent = descriptionInput.value;
-    popup.classList.remove('popup_opened');
-  }
-});
+  profileName.textContent = nameInput.value;
+  profileDescription.textContent = descriptionInput.value;
+  closePopup()
+}
 
+popupForm.addEventListener('submit', formSubmitHandler);
 
-closeButton.addEventListener('click', function () {
-  popup.classList.remove('popup_opened');
-});
-
-const likeButton = document.querySelectorAll('.element__button-like');
-
-likeButton.forEach(button => {
+for (let button of buttonLike) {
   button.addEventListener('click', function () {
-    button.classList.toggle('element__button-like');
     button.classList.toggle('element__button-like_active');
   });
-});
+}
