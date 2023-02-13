62e1357d650bcc07dd06772aa43f91e75b0349cf
Alexey Kazhanov <akazhanov72@yandex.ru>
2023-02-13 20:15:15 +0500

    Close button script added

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 const popup = document.querySelector('.popup');
 const popupProfileEdit = document.querySelector('.popup_profile-edit');
 const popupAddCard = document.querySelector('.popup_add-card')
-const buttonClose = document.querySelector('.popup__btn-close');
+const buttonClose = document.querySelectorAll('.popup__btn-close');
 const buttonEdit = document.querySelector('.profile__edit-button');
 const nameInput = document.querySelector('#name');
 const descriptionInput = document.querySelector('#description');
@@ -10,12 +10,31 @@ const profileDescription = document.querySelector('.profile__job');
 const popupForm = document.querySelector('.popup__form');
 const buttonAdd = document.querySelector('.profile__add-button');
 
-function closePopup() {
+function closePopup(buttonClose) {
+  const popup = buttonClose.closest('.popup');
+  popup.classList.remove('popup_opened');
+}
+
+buttonClose.forEach(function(buttonClose) {
+  buttonClose.addEventListener('click', function() {
+    closePopup(buttonClose);
+  });
+});
+
+/* buttonClose.forEach(function(closeBtn) {
+  closeBtn.addEventListener('click', function() {
+    const popup = closeBtn.closest('.popup');
+    popup.classList.remove('popup_opened');
+  });
+});
+*/
+
+/* function closePopup() {
   popup.classList.remove("popup_opened");
 }
 
 buttonClose.addEventListener("click", closePopup);
-
+*/
 function editPopup() {
   nameInput.value = profileName.textContent;
   descriptionInput.value = profileDescription.textContent;
